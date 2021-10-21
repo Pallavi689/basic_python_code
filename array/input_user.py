19496383445ee011fab0c64f7bc8635ee48bd82c
Pallavi689 <pallaviagrawal689@gmail.com>
2021-10-21 18:20:27 +0530

    get input from user

---
+++ array/input_user.py
@@ -0,0 +1,7 @@
+arr=[]
+#numbert of elements as input
+n=int(input('enter a size '))
+for i in range(0, n):
+    arr1=input('enter value of index a[{}] : '.format(i))
+    arr.append(arr1)
+print(arr)
