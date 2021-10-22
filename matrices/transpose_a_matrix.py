52bc4071e61048c680dbc7157ae5a8d8e551d1d7
Pallavi689 <pallaviagrawal689@gmail.com>
2021-10-22 18:29:33 +0530

    row to column

---
+++ matrices/transpose_a_matrix.py
@@ -0,0 +1,11 @@
+#if you change the rows of the matrix with the column of the same matrix it is known as the transpose matrix
+# a=[[1,3,5],[4,6,0],[2,9,6],[12,65,8]]
+# b=[[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+# for i in range(len(a)):
+#     for j in range(len(a[0])):
+#         b[j][i]=a[i][j]
+# for k in b:
+#     print(k)
+
+
+
